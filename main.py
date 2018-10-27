43a3d394a8535697714691d193f210f107bff3f0
Freek_Liang <liang66318@gmail.com>
2018-10-27 13:51:30 +0800

    [Update] Modify, Delete complete

--- main.py
+++ main.py
@@ -65,7 +65,8 @@ class ItemUploadHandler(webapp2.RequestHandler):
 		upload_price = self.request.get('product_price')
 		product = ItemData(item=upload_name, price=int(upload_price), pic=upload_file)
 		product.put()
-		self.redirect('/keyinproduct')
+		#self.redirect('/keyinproduct')
+		self.response.out.write('<meta http-equiv="refresh" content="0.1; url=http://localhost:8080/keyinproduct" />')
 		
 class PurchaseUploadHandler(webapp2.RequestHandler):
 	def post(self):
@@ -87,59 +88,71 @@ class PurchaseUploadHandler(webapp2.RequestHandler):
 		
 		product = PurchaseData(item=upload_item, amount=int(upload_amount), total=int(upload_total), moneyin=b_upload_moneyin, sold=b_upload_sold, buyer=upload_buyer, note=upload_note)
 		product.put()
-		self.redirect('/')
+		#self.redirect('/')
+		self.response.out.write('<meta http-equiv="refresh" content="0.1; url=http://localhost:8080/" />')
 		
 class PurchaseModifyHandler(webapp2.RequestHandler):
 	def post(self):
-		modify_id = self.request.get('modify_id')
-		modify_item_id = self.request.get('modify_item_id')
-		modify_buyer = self.request.get('modify_buyer')
-		modify_item = self.request.get('modify_item')
-		modify_amount = self.request.get('modify_amount')
-		modify_total = self.request.get('modify_total')
-		modify_moneyin = self.request.get('modify_moneyin')
-		modify_sold = self.request.get('modify_sold')
-		modify_note = self.request.get('modify_note')
+		modify_submit = self.request.get('modify_submit')
+		if modify_submit == 'Modify':
+			modify_id = self.request.get('modify_id')
+			modify_item_id = self.request.get('modify_item_id')
+			modify_buyer = self.request.get('modify_buyer')
+			modify_item = self.request.get('modify_item')
+			modify_amount = self.request.get('modify_amount')
+			modify_total = self.request.get('modify_total')
+			modify_moneyin = self.request.get('modify_moneyin')
+			modify_sold = self.request.get('modify_sold')
+			modify_note = self.request.get('modify_note')
+			
+			
+			b_modify_moneyin = False;
+			b_modify_sold = False;
+			if (modify_moneyin == "on"):
+				b_modify_moneyin = True;
+			if (modify_sold == "on"):
+				b_modify_sold = True;
+			
+			modify_purchase = PurchaseData.query(ancestor=ndb.Key(PurchaseData, int(modify_id))).get()
+			modify_purchase.buyer = modify_buyer
+			modify_purchase.item = modify_item_id
+			modify_purchase.amount = int(modify_amount)
+			modify_purchase.total = int(modify_total)
+			modify_purchase.moneyin = b_modify_moneyin
+			modify_purchase.sold = b_modify_sold
+			modify_purchase.note = modify_note
+			
+			modify_purchase.put()
 		
 		
-		b_modify_moneyin = False;
-		b_modify_sold = False;
-		if (modify_moneyin == "on"):
-			b_modify_moneyin = True;
-		if (modify_sold == "on"):
-			b_modify_sold = True;
+		elif modify_submit == 'Delete':
+			modify_id = self.request.get('modify_id')
+			modify_purchase = PurchaseData.query(ancestor=ndb.Key(PurchaseData, int(modify_id))).get()
+			modify_purchase.key.delete()
 		
-		modify_purchase = PurchaseData.query(ancestor=ndb.Key(PurchaseData, int(modify_id))).get()
-		modify_purchase.buyer = modify_buyer
-		modify_purchase.item = modify_item_id
-		modify_purchase.amount = int(modify_amount)
-		modify_purchase.total = int(modify_total)
-		modify_purchase.moneyin = b_modify_moneyin
-		modify_purchase.sold = b_modify_sold
-		modify_purchase.note = modify_note
 		
-		modify_purchase.put()
-		self.redirect('/')
+		#self.redirect('/')
+		self.response.out.write('<meta http-equiv="refresh" content="0.1; url=http://localhost:8080/" />')
 		
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 		
 		self.response.out.write("""<html><style>
-			h3{
-					width: 100%;
-					height: 30px;
-					margin: 0px;
-					float: center;
-					padding: 0px;
-					background-color: white;
+			#container {
+			  height: 100%;
+			  width: 100%;
+			  display: flex;
 			}
-			h4 {
-					float: right;
-					padding: 10px;
-					text-align: inherit;
-					background-color: white;
-				}
+			#output_form {
+			  width: 70%;
+			}
+			#input_form {
+			  width: 30%;
+			  float: right;
+			  padding: 10px;
+			  background-color: white;
+			}
 			table {
 				font-family: arial, sans-serif;
 				border-collapse: collapse;
@@ -154,17 +167,37 @@ class MainHandler(webapp2.RequestHandler):
 			tr:nth-child(even) {
 				background-color: #dddddd;
 			}
-		</style><body><table><br>""")
+		</style><body><div id="container"><div id="output_form"><table><br>""")
 		purchases = PurchaseData.query().order(PurchaseData.item)
+		products = ItemData.query().order(ItemData.item)
 		self.response.out.write("<tr><th>Buyer</th><th>Item</th><th>Amount</th><th>Total</th><th>MoneyIn</th><th>Export</th><th>Note</th><th>Modify</th><th>Delete</th></tr>")
 		for purchase in purchases:
-			self.response.out.write('<form action="/upload_modify" method="POST" enctype="multipart/form-data">')
-			self.response.out.write("<tr><input type='hidden' name='modify_id' value='%s'>" %(purchase.key.id()))
+			self.response.out.write('<tr><form action="/modify_purchase" method="POST" enctype="multipart/form-data">')
+			self.response.out.write("<input type='hidden' name='modify_id' value='%s'>" %(purchase.key.id()))
 			self.response.out.write("<input type='hidden' name='modify_item_id' value='%s'>" %(purchase.item))
-			self.response.out.write('<td><input type="text" name="modify_buyer" value="%s"></td>' %(purchase.buyer))
-			self.response.out.write('<td><input type="text" name="modify_item" value="%s"></td>' %(ItemData.query(ancestor=ndb.Key(ItemData, int(purchase.item))).get().item))
-			self.response.out.write('<td><input type="text" name="modify_amount" value="%s"></td>' %(purchase.amount))
-			self.response.out.write('<td><input type="text" name="modify_total" value="%s"></td>' %(purchase.total))
+			self.response.out.write('<td><input type="text" name="modify_buyer" value="%s" size="3"></td>' %(purchase.buyer))
+			
+			
+			self.response.out.write('<td><select style="width:100px;" name="modify_item" id="modify_item_%s" onChange="onSelectedFunc1(this)">' %(purchase.key.id()))
+			for product in products:	
+				if int(purchase.item) == product.key.id():
+					self.response.out.write('<option value="%s" selected>%s</option>' %(product.key.id(), product.item))
+				else:
+					self.response.out.write('<option value="%s">%s</option>' %(product.key.id(), product.item))
+			#self.response.out.write('<td><input type="text" name="modify_item" value="%s"></td>' %(ItemData.query(ancestor=ndb.Key(ItemData, int(purchase.item))).get().item))
+			self.response.out.write('</select></td>')
+			
+			self.response.out.write('<td><select name="modify_amount" id="modify_amount_%s" onChange="totalChangedFunc1(this)">' %(purchase.key.id()))
+			for x in range(1,11):
+				if int(purchase.amount) == x:
+					self.response.out.write('<option value="%s" selected>%s</option>' %(str(x),str(x)))
+				else:
+					self.response.out.write('<option value="%s">%s</option>' %(str(x),str(x)))
+			#self.response.out.write('<td><input type="text" name="modify_amount" value="%s"></td>' %(purchase.amount))
+			self.response.out.write('</select></td>')
+			
+			
+			self.response.out.write('<td><input type="text" id="modify_total_%s" name="modify_total" value="%s" size="3"></td>' %(purchase.key.id(), purchase.total))
 			if purchase.moneyin:
 				self.response.out.write('<td><input type="checkbox" name="modify_moneyin" checked></td>')
 			else:
@@ -174,46 +207,14 @@ class MainHandler(webapp2.RequestHandler):
 			else:
 				self.response.out.write('<td><input type="checkbox" name="modify_sold"></td>')
 				
-			self.response.out.write('<td><input type="text" name="modify_note" value="%s"></td>' %(purchase.note))
+			self.response.out.write('<td><input type="text" name="modify_note" value="%s" size="3"></td>' %(purchase.note))
 			self.response.out.write('<td><input type="submit" name="modify_submit" value="Modify"></td>')
 			self.response.out.write('<td><input type="submit" name="modify_submit" value="Delete"></td>')
-			self.response.out.write('</tr></form><br>')
-		
-		self.response.out.write("""</table><script type="text/javascript">\n""")
-		self.response.out.write('var single_price = 0;\nvar ProductList = {};\n')
-		
-		
-		products = ItemData.query().order(ItemData.item)
-		for product in products:
-			self.response.out.write('ProductList["%s"] = ' %(product.key.id()))
-			self.response.out.write('"data:image/png;base64,"+"%s";' %b64encode(product.pic))
-			self.response.out.write('ProductList["%s"+"_price"] = ' %(product.key.id()))
-			self.response.out.write('"%s";' %(product.price))
-		self.response.write('''
-							   function onSelectedFunc(onSelectedValue) {
-								   document.getElementById("showimg").src=ProductList[onSelectedValue.value];
-								   document.getElementById("product_price").textContent="Price: "+ProductList[onSelectedValue.value+"_price"];
-								   single_price = parseInt(ProductList[onSelectedValue.value+"_price"]);
-							   }
-							   function checkFile() {
-								   var fileElement = document.getElementById("upload_item");
-								   var fileElement1 = document.getElementById("upload_amount");
-								   var fileElement2 = document.getElementById("upload_total");
-								   var fileElement3 = document.getElementById("upload_buyer");
-								   if (fileElement.value != "" && fileElement1.value != "" && fileElement2.value != "" && fileElement3.value != "") {
-									   alert("File upload success!");
-									   return true;
-								   } else {
-									   alert("You must select a file or enter a name to upload!");
-									   return false;
-								   }
-							   }
-							   function totalChangedFunc(onSelectedAmount){
-								   var cost = parseInt(onSelectedAmount.value) * single_price;
-								   document.getElementById("upload_total").value = cost.toString();
-							   }
-							   </script>''')
-		self.response.out.write('<h4><form action="/upload_purchase" method="POST" enctype="multipart/form-data" onsubmit="return checkFile()"><select name="Product_item" id="upload_item" onChange="onSelectedFunc(this)"><option value="" selected disabled hidden>Choose here</option>')
+			self.response.out.write('</form></tr><br>')
+			
+			
+		self.response.out.write('</table></div>')
+		self.response.out.write('<div id="input_form"><form action="/upload_purchase" method="POST" enctype="multipart/form-data" onsubmit="return checkFile()"><select style="width:100px;" name="Product_item" id="upload_item" onChange="onSelectedFunc(this)"><option value="" selected disabled hidden>Choose here</option>')
 		
 		for product in products:	
 			self.response.out.write('<option value="%s">%s</option>' %(product.key.id(), product.item))
@@ -229,7 +230,63 @@ class MainHandler(webapp2.RequestHandler):
 			<input type="checkbox" name="checkbox_sold">sold<br>
 			Buyer: <input type="text" name="product_buyer" id="upload_buyer"><br>
 			Note: <input type="text" name="product_note"><br>
-			<input type="submit" name="submit" value="Submit"> </form></h4></body></html>""")
+			<input type="submit" name="submit" value="Submit"> </form></div>""")
+		
+		self.response.out.write("""<script type="text/javascript">\n""")
+		self.response.out.write('var single_price = 0;\nvar amount = 0;\nvar ProductList = {};\n')
+		
+		
+		for product in products:
+			self.response.out.write('ProductList["%s"] = ' %(product.key.id()))
+			self.response.out.write('"data:image/png;base64,"+"%s";' %b64encode(product.pic))
+			self.response.out.write('ProductList["%s"+"_price"] = ' %(product.key.id()))
+			self.response.out.write('"%s";' %(product.price))
+		self.response.write('''
+							   function onSelectedFunc(onSelectedValue) {
+								   document.getElementById("showimg").src=ProductList[onSelectedValue.value];
+								   document.getElementById("product_price").textContent="Price: "+ProductList[onSelectedValue.value+"_price"];
+								   single_price = parseInt(ProductList[onSelectedValue.value+"_price"]);
+								   if (amount>0){
+									var cost = amount * single_price;
+									document.getElementById("upload_total").value = cost.toString();
+								   }
+							   }
+							   function onSelectedFunc1(onSelectedValue) {
+								   var modify_form_id = onSelectedValue.id.split("item_")[1];
+								   var single_price1 = parseInt(ProductList[onSelectedValue.value+"_price"]);
+								   var amount_getid = document.getElementById("modify_amount_"+modify_form_id);
+								   var amount1 = parseInt(amount_getid.options[amount_getid.selectedIndex].value);
+								   var cost = amount1 * single_price1;
+								   document.getElementById("modify_total_"+modify_form_id).value = cost.toString();
+							   }
+							   function checkFile() {
+								   var fileElement = document.getElementById("upload_item");
+								   var fileElement1 = document.getElementById("upload_amount");
+								   var fileElement2 = document.getElementById("upload_total");
+								   var fileElement3 = document.getElementById("upload_buyer");
+								   if (fileElement.value != "" && fileElement1.value != "" && fileElement2.value != "" && fileElement3.value != "") {
+									   alert("File upload success!");
+									   return true;
+								   } else {
+									   alert("You must select a file or enter a name to upload!");
+									   return false;
+								   }
+							   }
+							   function totalChangedFunc(onSelectedAmount){
+								   amount = parseInt(onSelectedAmount.value);
+								   var cost = amount * single_price;
+								   document.getElementById("upload_total").value = cost.toString();
+							   }
+							   function totalChangedFunc1(onSelectedAmount){
+								   var modify_form_id = onSelectedAmount.id.split("amount_")[1];
+								   var amount1 = parseInt(onSelectedAmount.value);
+								   var item_getid = document.getElementById("modify_item_"+modify_form_id);
+								   var single_price1 = parseInt(ProductList[item_getid.options[item_getid.selectedIndex].value+"_price"]);
+								   var cost = amount1 * single_price1;
+								   document.getElementById("modify_total_"+modify_form_id).value = cost.toString();
+							   }
+							   </script></body></html>''')
+		
 		
 
 app = webapp2.WSGIApplication([
@@ -237,5 +294,5 @@ app = webapp2.WSGIApplication([
 	('/keyinproduct', ItemUploadFormHandler),
 	('/upload_item', ItemUploadHandler),
 	('/upload_purchase', PurchaseUploadHandler),
-	('/upload_modify', PurchaseModifyHandler)
+	('/modify_purchase', PurchaseModifyHandler)
 ], debug=True)
